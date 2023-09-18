92556526aec76bcbd8e386cc6f0bf0b4ffd0be22
André Victor <andrehsvictor@gmail.com>
2023-09-18 09:18:25 -0300

    feat: finished the class Cidade.java

--- Cidade.java
+++ Cidade.java
@@ -1,4 +1,36 @@
 
-public class Cidade {
+public class Cidade implements Comparable<Cidade> {
+    private String nome;
+    private int temp;
+    
+    public Cidade(String nome, int temp) {
+        this.nome = nome;
+        this.temp = temp;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTemp() {
+        return temp;
+    }
+
+    public void setTemp(int temp) {
+        this.temp = temp;
+    }
+
+    @Override
+    public int compareTo(Cidade c) {
+        return this.nome.compareTo(c.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "nome: " + this.nome + " temp: " + this.temp;
+    }
 }
